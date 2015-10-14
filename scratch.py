083d05d9706cd3301b4c60331f315778b83b53c0
Soumya <sds2p8@mail.missouri.edu>
2015-10-13 23:22:12 -0400

    .

---
+++ scratch.py
@@ -0,0 +1,6 @@
+      <table class="table table-hover">
+      <tr><th>Office-based charges</th><th>Outpatient charges</th><th>Inpatient charges</th></tr><tr><th>Emergency room charges</th><tr>
+      <tr><td>{{ values["office"] }}</td><td>{{ values["outpatient"]}}</td><td> {{ values["inpatient"] }}</td><td> {{ values["er"] }}</td></tr>
+      </table>
+
+
